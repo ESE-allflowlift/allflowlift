90b9603f07a2112cc5473bd07540580c05cc7db3
jelle huis in t veld <j_hitv@hotmail.com>
2011-10-13 09:42:13 +0200

    functies toegevoegd in functions_fsm

--- functions_fsm.c
+++ functions_fsm.c
@@ -13,32 +13,108 @@
 
 void f_stop_pomp(int * s_pomp_active) 
 {
+	f_eeprom_looptijd;
 	*(s_pomp_active+0) = 0;
 	*(s_pomp_active+1) = 0;
-	f_eeprom_looptijd
+
 }
 
 /** 
  * fuctie start pomp 
+ * @param start allen een pomp zonder error, degene met de minste draai uren
  * @param s_pomp_active array van actieve pompen
  * @param z_pomp_looptijd array met looptijden van beide pompen
  * @param t_looptijd timer van maximale looptijd van de pomp
  * @param s_pomp_error staat er een pomp in error
  */
 
-void f_start_pomp(int * s_pomp_active,const long int * z_pomp_looptijd,int * t_looptijd,int * s_pomp_error)
+void f_start_pomp(int * s_pomp_active,const long int * z_pomp_looptijd,int * t_looptijd,const int * s_pomp_error)
 {
-	// de error ding moet er nog bij	
-	if (*(z_pomp_looptijd+0) <= *(z_pomp_looptijd+1))
-	{
-		*(s_pomp_active+0) = 1;
-	}
-	else 
-	{
-		*(s_pomp_active+1) = 1;	
-	}
-	*(t_looptijd) = 0;    /* er moet nog een functie start timer gemaakt worden */
+	*(t_looptijd) = 0; 
+			if (*(z_pomp_looptijd+0) <= *(z_pomp_looptijd+1) || (* s_pomp_error+1 < 1) && (* s_pomp_error+0 < 1))
+			{
+				*(s_pomp_active+0) = 1;
+			}								/*start pomp 1*/
+			if (*(z_pomp_looptijd+0) > *(z_pomp_looptijd+1) || (* s_pomp_error+0 < 1) && (* s_pomp_error+1 < 1))
+			{
+				*(s_pomp_active+1) = 1;	
+			}								/*start pomp 2*/
+			else;  /*allebij in error, geen pomp start*/
+	f_eeprom_inschakelingen
+	  /* er moet nog een functie start timer gemaakt worden */
 	/* f_starttimer (maakt gebruik van hardware timer's en interupts*/
-	
+
+
+
+
+/**
+ *functie f_eeprom_looptijd onthoud de looptijded 
+ * @param t_looptijd huidige looptijd actieve pomp
+ * @param z_pomp_looptijd looptijden van bijde pompen
+ * @param s_pomp_error array van actieve pompen
+ * 
+ */
+
+void f_eeprom_looptijd (const int * s_pomp_active,long int * z_pomp_looptijd,int * t_looptijd)
+{	
+	if (*(s_pomp_active+0) = 1 && *(t_looptijd) > 0)
+	{
+	*(z_pomp_looptijd+0) = *(t_looptijd+0) + *(z_pomp_looptijd+0); 
+	}
+	if (*(s_pomp_active+1) = 1 && *(t_looptijd) > 0)
+	{	
+	*(z_pomp_looptijd+1) = *(t_looptijd) + *(z_pomp_looptijd+1); 
+	}
+	else;
+	*(t_looptijd) = 0;
+/* sla z_pomp_looptijd op op het eeprom */
 }
+
+/**
+ *functie f_eeprom_inschakelingen onthoud het aantal inschakelingen 
+ * t_looptijd huidige looptijd actieve pomp
+ * z_pomp_inschakelingen array looptijden van bijde pompen
+ * s_pomp_error array van actieve pompen
+ * 
+ */
+
+void f_eeprom_looptijd (const int * s_pomp_active,int * z_pomp_inschakelingen)
+{
+	if (*(s_pomp_active+0) = 1)
+	{
+	*(z_pomp_inschakelingen+0)++
+	} 
+	if (*(s_pomp_active+1) = 1)
+	{
+	*(z_pomp_inschakelingen+1)++
+	}
+	else;
+/* sla z_pomp_inschakelingen op op het eeprom */
+}
+
+/**
+ *functie f_pomp_seterror zet pomp in error als dat nodig is 
+ * @param s_pomp_active array van actieve pomp
+ * @param s_pomp_error	array van pomp in error
+ * @param e_motor_fase array motor fase verkeerd
+ * @param e_motor_stroom array motor stroom verkeerd
+ * @param e_motor_temp array motor tempratuur verkeerd
+ * 
+ */
+
+void f_pomp_seterror (*(int s_pomp_error), *(s_pomp_active), *( )
+
+
+
+
+
+
+
+
+
+
 #endif
+
+
+
+
