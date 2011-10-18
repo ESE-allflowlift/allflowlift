8f461506ce8649e7b3bee39ba411781a2848d46d
Randy Wijnants <catch@tty32.org>
2011-10-18 14:33:44 +0200

    main.c geupdate

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include <avr/io.h>
+#include <util/delay.h>
 
 #include "variables.h"
 #include "functions_fsm.h"
+#include "hardware_declarations.h"
 
 int main(void) {
+
+	init_pins();
+	
 	s_pomp_active[0] = 1;
 	s_pomp_active[1] = 1;
 	printf("%d - %d\n", s_pomp_active[0], s_pomp_active[1]);
